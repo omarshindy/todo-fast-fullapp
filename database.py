6c003e87e854bae4e154f7a127225f6dccc2fa7f
Omar Shindy <shindi.omar@gmail.com>
2022-03-30 15:37:30 +0200

    Fixing Configuration for Postgresql production Database

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
 if SQLALCHEMY_DATABASE_URL.startswith('postgres://'):
-    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace('postgre://', 'postgresql://', 1)
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace('postgres://', 'postgresql://', 1)
 # MYSQL Series
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/todoapp"
 
